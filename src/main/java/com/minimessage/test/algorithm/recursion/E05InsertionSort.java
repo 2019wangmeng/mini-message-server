1f95f285e87c68e83551df4dbf623b6a8ca554a6
wangmeng <1021385407@qq.com>
2025-02-03 22:33:13 +0800

    递归实现插入排序

---
+++ src/main/java/com/minimessage/test/algorithm/recursion/E05InsertionSort.java
@@ -0,0 +1,25 @@
+package com.minimessage.test.algorithm.recursion;
+
+/**
+ * 插入排序
+ */
+public class E05InsertionSort {
+
+    public static void sort(int[] a) {
+        insertion(a, 1);
+    }
+
+    private static void insertion(int[] a, int low) {
+        if (low == a.length) {
+            return;
+        }
+        int t = a[low];
+        int i = low - 1;
+        while (i >= 0 && a[i] > t) {
+            a[i + 1] = a[i];
+            i--;
+        }
+        a[i + 1] = t;
+        insertion(a, low + 1);
+    }
+}
